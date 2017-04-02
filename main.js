b89d07d9e25e44e84757d4bd7186bc7b7a7255c6
NivBraz <nivbraz90@gmail.com>
2017-04-02 18:24:23 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,10 @@
 var express = require('express');
 var app = express();
-var events = require('events');
 var ws_module = require('./ws_module');
-var eventEmitter = new events.EventEmitter();
 var log = '';
 var player1 = ws_module("niv" , 10);
 var player2 = ws_module("braz" , 0);
 
-// listener #1
-var listnerAdd = function listnerAdd() {
-   log += 'addGoal executed.';
-}
-
-// listener #2
-var listnerRev = function listnerRev() {
-  log += 'removeGoal executed.';
-}
-
-eventEmitter.on('addGoal', listnerAdd);
-eventEmitter.on('removeGoal', listnerRev);
 
 player1.on('addGoal',function() {
     log += ("addGoal executed to: " + this.name + 
@@ -44,17 +30,16 @@ player2.addGoal();
 player1.addGoal();
 player2.removeGoal();
 
-eventEmitter.emit('addGoal');
 
 app.get('/', function (req, res) {
    res.send(log);
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
 
-console.log(log);
+//console.log(log);
